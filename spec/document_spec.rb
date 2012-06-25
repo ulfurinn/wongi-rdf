b43121e84884f68c126560b9a2de6dbdb99f85f3
Valeri Sokolov <ulfurinn@ulfurinn.net>
2012-06-25 17:07:11 +0200

    some document tests re: resource creation

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -3,18 +3,29 @@ require 'wongi-rdf/document'
 
 describe Wongi::RDF::Document do
 
+  subject {
+    Wongi::RDF::Document.new
+  }
+
   it 'should be empty initially' do
-    document = Wongi::RDF::Document.new
-    document.should be_empty
-    document.statements.should be_empty
-    document.namespaces.should be_empty
+    should be_empty
+    subject.statements.should be_empty
+    subject.namespaces.should be_empty
   end
 
   it 'should refuse to replace its base' do
-    document = Wongi::RDF::Document.new
-    document.base = URI.parse( 'http://test/base/' )
-    lambda { document.base = URI.parse( 'http://test/base/' ) }.should_not raise_error(Wongi::RDF::BaseException)
-    lambda { document.base = URI.parse( 'http://test/base1/' ) }.should raise_error(Wongi::RDF::BaseException)
+    subject.base = URI.parse( 'http://test/base/' )
+    lambda { subject.base = URI.parse( 'http://test/base/' ) }.should_not raise_error(Wongi::RDF::BaseException)
+    lambda { subject.base = URI.parse( 'http://test/base1/' ) }.should raise_error(Wongi::RDF::BaseException)
+  end
+
+  it 'should create a resource from a URI' do
+    resource = subject.resource URI.parse( 'http://test/resource' )
+    resource.should be_a_kind_of( Wongi::RDF::Resource )
+  end
+
+  it 'should not create a resource from bad arguments' do
+    lambda { subject.resource 42 }.should raise_error
   end
 
 end
